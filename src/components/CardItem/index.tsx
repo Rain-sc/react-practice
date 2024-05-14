de70c2c9cd7318168a155fb498bf57ea75ffc539
Rain <shunchitrain@gmail.com>
2024-05-14 16:39:05 +0800

    alter dashboard of card item style

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -1,21 +1,37 @@
-import { Card, Statistic, type StatisticProps } from "antd"
+import { Card, Image, Statistic, type StatisticProps } from "antd"
+import styles from './index.module.scss'
 
 type CardItemProps = {
   title: string
   value: number | string
-  formatter: StatisticProps['formatter']
+  formatter: StatisticProps['formatter'],
+  image: string
 }
-const CardItem = ({ title, value, formatter }: CardItemProps) => {
+const CardItem = ({ title, value, formatter, image }: CardItemProps) => {
+
   return (
-    <Card bordered={false}>
-      <Statistic
-        title={title}
-        value={value}
-        formatter={formatter}
-        valueStyle={{
-          color: '#3f8600',
-        }}
-      />
+    <Card
+      bordered={false}
+      hoverable
+      className={styles['ant-statistic']}
+    >
+      <div className="flex items-center justify-between">
+        <Statistic
+          title={title}
+          value={value}
+          formatter={formatter}
+          valueStyle={{
+            color: '#464255',
+            fontSize: 34,
+            fontWeight: 600
+          }}
+        />
+        <Image
+          src={image}
+          width={35}
+          preview={false}
+        />
+      </div>
     </Card>
   )
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { UserStoreType } from "@/store/modules/user"
 import { Col, Row, type StatisticProps } from "antd";
 import CardItem from '@/components/CardItem';
+import followerIcon from '@/assets/images/dashboard/followers.svg'
+import articleIcon from '@/assets/images/dashboard/articles.svg'
 
 const formatter: StatisticProps['formatter'] = (value) => (
   <CountUp end={value as number} separator="," />
@@ -11,18 +13,20 @@ const Dashboard = () => {
   const userInfo = useSelector((state: UserStoreType) => state.user.useInfo)
   return (
     <Row gutter={24}>
-      <Col span={6}>
+      <Col span={8}>
         <CardItem
           title="The Number of Article"
           value={userInfo.art_count}
           formatter={formatter}
+          image={articleIcon}
         />
       </Col>
-      <Col span={6}>
+      <Col span={8}>
         <CardItem
           title="The Number of Follower"
           value={userInfo.art_count}
           formatter={formatter}
+          image={followerIcon}
         />
       </Col>
     </Row>
